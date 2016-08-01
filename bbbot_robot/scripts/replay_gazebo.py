4a70e0512364c95e509ab82644066a04f9b37c9a
lonewolf <nikhilkalige@gmail.com>
2016-07-31 17:21:56 -0700

    Remove unused functions

--- bbbot_robot/scripts/replay_gazebo.py
+++ bbbot_robot/scripts/replay_gazebo.py
@@ -1,33 +1,12 @@
 #! /usr/bin/env python
-from bbbot_robot.cmaes import setup_cmaes, run_cmaes, checkpoint_handle, generate_name, init_creator
-from bbbot_robot.cmaes_hansen import hans_setup_cmaes, hans_run_cmaes
-from bbbot_robot.evaluate import Evaluate, EvaluateHansen, EvaluateGazebo, PIdx
-from bbbot_robot.robot import Robot
-from bbbot_robot.robot import JointNames as JN
+from bbbot_robot.evaluate import EvaluateGazebo
 import sys
 import os
 import rospy
 
 
-cmaes_dump = "/home/nikhilkalige/workspace/asu/thesis/cmaes_dump"
-NGEN = 1000
-SIGMA = 1
 DMP = 15
 BAG_FILE = '/home/nikhilkalige/workspace/ros/devel_packages/bag_files/2016-05-15-15-20-40.bag'
-
-
-def cmaes_hansen(robot, location):
-    # cmaes = hans_setup_cmaes(SIGMA, robot.get_initial_params(), constraint_func=True)
-
-    # p = robot.get_initial_params()
-    # robot.eval(tbest)
-    # return
-    # filename = '/home/lonewolf/workspace/asu/thesis/cmaes_dump/12_06_15_19_21/cma_00021.pkl'
-
-    filename = '/home/nikhilkalige/workspace/asu/thesis/cmaes_dump/26_07_18_18_00/cma_00008.pkl'
-    cmaes = hans_setup_cmaes(SIGMA, robot.get_initial_params(), constraint_func=True, checkpoint=True, filename=filename)
-    robot.set_evals(cmaes.countevals)
-    hans_run_cmaes(cmaes, robot.eval, location)
 
 
 if __name__ == '__main__':
