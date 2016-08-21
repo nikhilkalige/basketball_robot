2c61d450ba1bce933ce8a9dd4d5b1d063db9187a
lonewolf <nikhilkalige@gmail.com>
2016-08-21 16:50:05 -0700

    Add new basketball tracker for gazebo

--- bbbot_robot/src/bbbot_robot/ball_tracker.py
+++ bbbot_robot/src/bbbot_robot/ball_tracker.py
@@ -1,6 +1,7 @@
 import threading
 import rospy
 from gazebo_msgs.msg import ModelStates
+import numpy as np
 
 
 class BallTracker:
@@ -55,6 +56,64 @@ class BallTracker:
     def get_reward(self):
         print (self.distance * 100) if self.distance > 0.4 else 0
         return (self.distance * 100) if self.distance > 0.4 else 0
+
+
+class BasketTracker(BallTracker):
+    X_POSITION = 2.5
+    Z_POSITION = 1.6
+    Z_HOOP_HEIGHT = 1.2
+
+    def model_cb(self, data):
+        if not self.running:
+            return
+        try:
+            idx = data.name.index("basketball")
+        except ValueError:
+            return
+        pos = data.pose[idx]
+        # print(pos.position.x, pos.position.z)
+        self.position.append([pos.position.x, pos.position.z])
+
+    def start(self):
+        self.position = []
+        self.distance = 0
+        self.z = 10
+        self.first_read = True
+        self.running = True
+
+    def get_reward(self):
+        positions = np.array(self.position)
+
+        # Check if there is a dunk
+        if positions[:, 1].min() >= 1.2:
+            print("Robot has dunked the ball")
+            return 0
+
+        # See if ball already on the ground
+        if positions[0][1] < 0.65:
+            return 800
+
+        dist = distance(positions, self.X_POSITION, self.Z_POSITION)
+        # The ball on the ground usually returns a value of 1.47, so any ball that falls
+        # very close to the robot and then rolls over gets the same reward, therefore we
+        # use find the point where the ball hits the ground and then calculate the distance
+        # from there.
+        if dist > 1.46:
+            z = positions[:, 1]
+            zd = np.diff(z)
+            stop_idx = zd.argmin()
+            xm, zm = positions[stop_idx]
+            sdist = np.linalg.norm(positions[stop_idx] - np.array([self.X_POSITION, self.Z_POSITION]))
+            dist = max(dist, sdist)
+
+        print(dist * 100)
+        return dist * 100
+
+
+def distance(array, x, z):
+    abs_point = np.array([x, z])
+    dist = np.linalg.norm(array - abs_point, axis=1)
+    return dist.min()
 
 
 if __name__ == '__main__':
